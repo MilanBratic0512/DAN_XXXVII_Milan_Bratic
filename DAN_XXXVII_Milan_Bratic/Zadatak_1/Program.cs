564a8e4e6282b7240b1faeedf03d966a2b659c35
MilanBratic0512 <milan.bratic2@gmail.com>
2020-06-23 09:45:43 +0200

    create routes method

--- DAN_XXXVII_Milan_Bratic/Zadatak_1/Program.cs
+++ DAN_XXXVII_Milan_Bratic/Zadatak_1/Program.cs
@@ -1,13 +1,40 @@
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Zadatak_1
 {
     class Program
     {
+        static string path = "../../Routes.txt";
+        static Random rnd = new Random();
+        static object locker = new object();
+        static void Routes()
+        {
+            lock (locker)
+            {
+                int[] routes = new int[1000];
+                for (int i = 0; i < 1000; i++)
+                {
+                    routes[i] = rnd.Next(1, 5001);
+                }
+                using (TextWriter tw = new StreamWriter(path))
+                {
+                    for (int i = 0; i < routes.Length; i++)
+                    {
+                        tw.WriteLine(routes[i]);
+                    }
+                }
+                Monitor.Pulse(locker);
+            }
+
+        }
+
+       
         static void Main(string[] args)
         {
 
