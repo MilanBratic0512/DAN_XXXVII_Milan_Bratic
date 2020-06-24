0c30e48e6e9c570e3e1ffc447f81570cc581f579
MilanBratic0512 <milan.bratic2@gmail.com>
2020-06-24 13:38:09 +0200

    commenting and completing application

--- DAN_XXXVII_Milan_Bratic/Zadatak_1/Program.cs
+++ DAN_XXXVII_Milan_Bratic/Zadatak_1/Program.cs
@@ -14,10 +14,10 @@ namespace Zadatak_1
         static Random rnd = new Random();
         static object locker = new object();
         static int[] bestRoutes = new int[10];
-        static Thread[] trucks = new Thread[10];
-        static Dictionary<string, int> TrucksAndLoadingTime = new Dictionary<string, int>();
-        static Dictionary<string, int> TrucksAndDeliveryTime = new Dictionary<string, int>();
         static SemaphoreSlim semaphore = new SemaphoreSlim(2, 2);
+        /// <summary>
+        /// method for choose and write routes to the file
+        /// </summary>
         static void Routes()
         {
             lock (locker)
@@ -34,25 +34,35 @@ namespace Zadatak_1
                         tw.WriteLine(routes[i]);
                     }
                 }
+                //when routes are choosen Monitor.Puls gives a signal to the Manager
                 Monitor.Pulse(locker);
             }
 
         }
 
+        /// <summary>
+        ///  method for pick out best routes
+        /// </summary>
         static void Manager()
         {
             lock (locker)
             {
+                //wait until routes are written to the file
                 Monitor.Wait(locker, rnd.Next(0, 3001));
 
+                //list of all routes
                 List<int> allRoutes = new List<int>();
+                //all routes divisible by three
                 List<int> routesDivisibleByThree = new List<int>();
+                //array of strings, fill from the file
                 string[] lines = File.ReadAllLines(path);
+                //place route to the list
                 foreach (var item in lines)
                 {
                     allRoutes.Add(int.Parse(item));
                 }
 
+                //choose routes divisible by three
                 for (int i = 0; i < allRoutes.Count; i++)
                 {
                     if (allRoutes[i] % 3 == 0)
@@ -60,45 +70,56 @@ namespace Zadatak_1
                         routesDivisibleByThree.Add(allRoutes[i]);
                     }
                 }
+                //sort routes 
                 routesDivisibleByThree.Sort();
+                //pick out ten best routes
                 for (int i = 0; i < bestRoutes.Length; i++)
                 {
                     bestRoutes[i] = routesDivisibleByThree[i];
                 }
                 Console.WriteLine("Routes are selected:");
+                //write on the console best routes
                 for (int i = 0; i < bestRoutes.Length; i++)
                 {
                     Console.WriteLine(bestRoutes[i]);
                 }
             }
         }
-
-        static void TruckLoading(int i, int time)
+        /// <summary>
+        /// method for truck loading
+        /// </summary>
+        /// <param name="truckName">represent name of truck</param>
+        /// <param name="time">truck loading time</param>
+        static void TruckLoading(int truckName, int time)
         {
 
             semaphore.Wait();
-            Console.WriteLine("The truck Truck_{0} loading", i);
+            Console.WriteLine("The truck Truck_{0} loading", truckName);
             Thread.Sleep(time);
-
-
-
             semaphore.Release();
 
-            Console.WriteLine("The truck Truck_{0} is loaded for {1}ms", i, time);
+            Console.WriteLine("The truck Truck_{0} is loaded for {1}ms", truckName, time);
 
         }
-        static void TruckUnloading(int i, int time, int route, int loadingTime)
+        /// <summary>
+        /// method for truck unloading
+        /// </summary>
+        /// <param name="truckName">pass truck name</param>
+        /// <param name="time">delivery time</param>
+        /// <param name="route">route</param>
+        /// <param name="loadingTime">loading time</param>
+        static void TruckUnloading(int truckName, int time, int route, int loadingTime)
         {
-            Console.WriteLine("The truck Truck_{0} is on its way to route {1}, delivery can be expected for {2}", i, route, time);
+            Console.WriteLine("The truck Truck_{0} is on its way to route {1}, delivery can be expected for {2}", truckName, route, time);
             if (time > 3000)
             {
-                Console.WriteLine("The truck Truck_{0} was canceled. Delivery time is too long", i);
+                Console.WriteLine("The truck Truck_{0} was canceled. Delivery time is too long", truckName);
                 return;
             }
             else
             {
                 Thread.Sleep(time);
-                Console.WriteLine("The truck Truck_{0} has arrived at its destination, it is unloading time: {1}", i, loadingTime / 1.5);
+                Console.WriteLine("The truck Truck_{0} has arrived at its destination, it is unloading time: {1:0.00}ms", truckName, loadingTime / 1.5);
             }
 
 
@@ -107,6 +128,7 @@ namespace Zadatak_1
         {
             Thread thread1 = new Thread(Routes);
             Thread thread2 = new Thread(Manager);
+            //place loading times into array
             int[] loadingTimes = new int[10];
             thread2.Start();
             thread1.Start();
@@ -115,6 +137,7 @@ namespace Zadatak_1
             for (int i = 0; i < 5; i++)
             {
                 int time1 = rnd.Next(500, 5000);
+                //since we have five iterations, in this way we ensure that ten threads enter the method
                 loadingTimes[2 * i] = time1;
                 int time2 = rnd.Next(500, 5000);
                 loadingTimes[2 * i + 1] = time2;
